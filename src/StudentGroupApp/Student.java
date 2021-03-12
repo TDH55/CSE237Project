d8bd234556b1105fa6019aa0b8ceaed46e3bac44
TDH55 <taylorhoward1@wustl.edu>
2021-03-11 21:44:51 -0600

    Added getters to student class

--- src/StudentGroupApp/Student.java
+++ src/StudentGroupApp/Student.java
@@ -1,5 +1,26 @@
 package StudentGroupApp;
 
 public class Student {
-	
+	private String name;
+	private String email;
+	private Integer classYear;
+	//TODO: add a list of events
+
+	public Student(String name, String email, Integer classYear) {
+	    this.name = name;
+	    this.email = email;
+	    this.classYear = classYear;
+    }
+
+    public String getName(){
+		return name;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public Integer getClassYear() {
+		return classYear;
+	}
 }
