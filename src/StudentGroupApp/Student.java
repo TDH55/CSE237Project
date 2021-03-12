e89fb24ad2016c5461eb7a395b75d819ea2cc586
TDH55 <taylorhoward1@wustl.edu>
2021-03-11 22:10:29 -0600

    Added setters to the student class

--- src/StudentGroupApp/Student.java
+++ src/StudentGroupApp/Student.java
@@ -1,17 +1,22 @@
 package StudentGroupApp;
 
+//The student class contains all the information about a student that is created upon launching the app
+
 public class Student {
+	//member variables for student class
 	private String name;
 	private String email;
 	private Integer classYear;
 	//TODO: add a list of events
 
+	//initializer for student class
 	public Student(String name, String email, Integer classYear) {
 	    this.name = name;
 	    this.email = email;
 	    this.classYear = classYear;
     }
 
+    //getters for student classs
     public String getName(){
 		return name;
 	}
@@ -22,5 +27,18 @@ public class Student {
 
 	public Integer getClassYear() {
 		return classYear;
+	}
+
+	//Setters for student class
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setClassYear(Integer classYear) {
+		this.classYear = classYear;
 	}
 }
--- src/Tests/StudentTests.java
+++ src/Tests/StudentTests.java
@@ -2,13 +2,53 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import StudentGroupApp.Student;
 import org.junit.jupiter.api.Test;
 
 class StudentTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetName() {
+		String name = "Taylor";
+		Student student = new Student(name, "email@email.com", 2022);
+		assertEquals(student.getName(), name);
 	}
 
+	@Test
+	void testGetEmail(){
+		String email = "email@email.com";
+		Student student = new Student("Taylor", email, 2022);
+		assertEquals(student.getEmail(), email);
+	}
+
+	@Test
+	void testGetClassYear(){
+		Integer classYear = 2022;
+		Student student = new Student("Taylor", "email@email.com", classYear);
+		assertEquals(student.getClassYear(), classYear);
+	}
+
+	@Test
+	void testSetName(){
+		String newName = "Taylor";
+		Student student = new Student("name", "email@email.com", 2022);
+		student.setName(newName);
+		assertEquals(student.getName(), newName);
+	}
+
+	@Test
+	void testSetEmail(){
+		String newEmail = "email@wustl.edu";
+		Student student = new Student("Taylor", "email@email.com", 2022);
+		student.setEmail(newEmail);
+		assertEquals(student.getEmail(), newEmail);
+	}
+
+	@Test
+	void testSetClassYear(){
+		Integer newClassYear = 2023;
+		Student student = new Student("Taylor", "email@wustl.edu", 2022);
+		student.setClassYear(newClassYear);
+		assertEquals(student.getClassYear(), newClassYear);
+	}
 }
