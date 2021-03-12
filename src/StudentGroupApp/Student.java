06551a176604843e82ee41406705bd33d9f697bc
TDH55 <taylorhoward1@wustl.edu>
2021-03-11 21:29:23 -0600

    fixed student group setup

--- src/StudentGroupApp/Student.java
+++ src/StudentGroupApp/Student.java
@@ -2,14 +2,4 @@ package StudentGroupApp;
 
 public class Student {
 	
-	private String name;
-	private String email;
-	private Integer classYear;
-	
-	public Student(String name, String email, Integer classYear) {
-		this.name = name;
-		this.email = email;
-		this.classYear = classYear;
-	}
-	
 }
