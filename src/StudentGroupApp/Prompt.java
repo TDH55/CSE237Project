3bf54bc32c05b31c5c83b180fa917c25047aaa80
KBell99 <31802760+KBell99@users.noreply.github.com>
2021-04-14 13:08:20 -0500

    students can create private groups

--- src/StudentGroupApp/Prompt.java
+++ src/StudentGroupApp/Prompt.java
@@ -507,6 +507,7 @@ public class Prompt {
 	
 	//create a new student group with current user as owner of group
 	private StudentGroup createStudentGroup() {
+		boolean isPrivate = false;
 		System.out.println("Please input a name for the group you would like to create:");
 		String groupName = this.keyboardIn.nextLine();
 		System.out.println("Please input a description of this student group: ");
@@ -514,8 +515,14 @@ public class Prompt {
 		System.out.println("Please select a tag for your group");
 		printTagChoices();
 		Tag groupTag = chooseTag();
+		System.out.println("Is this group private? (Y/N)");
+		String isPrivateQuery = this.keyboardIn.nextLine();
+		
+		if(isPrivateQuery.equals("Y") || isPrivateQuery.equals("y")) {
+			isPrivate = true;
+		}
 
-		StudentGroup newGroup = new StudentGroup(groupName, groupDescription, currentStudent, groupTag);
+		StudentGroup newGroup = new StudentGroup(groupName, groupDescription, currentStudent, isPrivate, groupTag);
 
 		this.addStudentGroupToList(newGroup);
 
